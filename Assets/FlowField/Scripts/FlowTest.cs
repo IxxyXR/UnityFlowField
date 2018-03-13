86aefafd3548b8183bb12cf4425aa064cc77d812
andybak <andy@andybak.net>
2018-03-13 10:26:44 +0000

    Initial commit

---
+++ Assets/FlowField/Scripts/FlowTest.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using UnityEditorInternal;
+using UnityEngine;
+
+[RequireComponent(typeof(ParticleSystem))]
+public class FlowTest : MonoBehaviour {
+
+	public bool AutoAdjustColor = false;
+	public bool UseTriggerVolume = false;
+	public bool UseTrails = true;
+	public Texture2D FlowMap;
+	public Vector2 offset = new Vector2(1,1);
+	public Vector2 scale = new Vector2(50,50);
+	public int preset = 0;
+	
+	private ParticleSystem system;
+	private ParticleSystem.Particle[] particles;
+	private float maxVelocity = 0f;
+	private float TOLERANCE = 0.1f;
+
+	void Start() {
+		if (system == null) {
+			system = GetComponent<ParticleSystem>();
+		}
+	}
+
+	private void LateUpdate () {
+		if (particles == null || particles.Length < system.maxParticles) {
+			particles = new ParticleSystem.Particle[system.maxParticles];
+		}
+		
+		var trigger = system.trigger;
+		trigger.enabled = UseTriggerVolume;
+		var trails = system.trails;
+		trails.enabled = UseTrails;
+		
+		int particleCount = system.GetParticles(particles);
+		PositionParticles();
+		system.SetParticles(particles, particleCount);
+	}
+	
+	private void PositionParticles () {
+		
+		for (int i = 0; i < particles.Length; i++) {
+			
+			var p = particles[i].position;
+
+			if (FlowMap != null) {
+				var pix = FlowMap.GetPixel((int) ((p.x + offset.x) * scale.x), (int) ((p.y + offset.y) * scale.y));
+				particles[i].velocity = new Vector3(
+					-0.5f + pix.r,
+					-0.5f + pix.g,
+					-0.5f + pix.b
+				);
+			}
+
+			switch (preset) {
+				case 0:
+					break;
+				case 1:
+					particles[i].velocity = new Vector3(
+						p.y/Mathf.Cos(p.magnitude),
+						Mathf.Max((Mathf.Log(p.y)+p.x),p.x),
+						p.z/Mathf.Sin(p.magnitude)
+					);
+					break;
+				case 2:
+					particles[i].velocity = new Vector3(
+						p.y,
+						Mathf.Cos(
+							Mathf.Min(
+								p.x/p.magnitude*Mathf.Cos(p.y),
+								p.y
+							)
+						),
+						0.2f
+					);
+					break;
+			}
+			
+			if (AutoAdjustColor) {
+				maxVelocity = Mathf.Max(
+					maxVelocity,
+					particles[i].velocity.x,
+					particles[i].velocity.y,
+					particles[i].velocity.z
+				);
+			}
+			
+			
+		}
+
+		if (AutoAdjustColor) {
+			if (Math.Abs(system.colorBySpeed.range[1] - maxVelocity) > TOLERANCE) {
+				var col = system.colorBySpeed;
+				col.range = new Vector2(0, maxVelocity);
+			}
+		}
+	}
+	
+}
---
+++ Assets/ParticleFlow.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleFlow : MonoBehaviour
+{
+	public Transform target;
+	public float force = 10.0f;
+
+	ParticleSystem ps;
+
+	void Start() {
+		ps = GetComponent<ParticleSystem>();
+	}
+
+	void LateUpdate() {
+		
+		ParticleSystem.Particle[] particles = new ParticleSystem.Particle[ps.particleCount];
+
+		ps.GetParticles(particles);
+
+		for (int i = 0; i < particles.Length; i++) {
+			
+			ParticleSystem.Particle p = particles[i];
+
+			Vector3 particleWorldPosition;
+
+			if (ps.main.simulationSpace == ParticleSystemSimulationSpace.Local) {
+				particleWorldPosition = transform.TransformPoint(p.position);
+			} else if (ps.main.simulationSpace == ParticleSystemSimulationSpace.Custom) {
+				particleWorldPosition = ps.main.customSimulationSpace.TransformPoint(p.position);
+			} else {
+				particleWorldPosition = p.position;
+			}
+
+			Vector3 directionToTarget = (target.position - particleWorldPosition).normalized;
+			Vector3 seekForce = (directionToTarget * force) * Time.deltaTime;
+
+			p.velocity += seekForce;
+
+			particles[i] = p;
+		}
+
+		ps.SetParticles(particles, particles.Length);
+		
+	}
+}
